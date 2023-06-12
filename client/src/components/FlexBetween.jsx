deeabf6f6c461359a5aa307bd42e8d2cb056b81f
abdabTheCreator <abby.crimlis@outlook.com>
2023-06-12 21:04:48 +1200

    first commit

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+const { Box } = require("@mui/material");
+
+const FlexBetween = styled(Box)({
+    display:"flex",
+    justifyContent:"space-between",
+    alignItems:"center"
+})
+
+export default FlexBetween;
---
+++ server/models/ProductStat.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+//this would be the aggregate query storing structure
+const ProductStatSchema = new mongoose.Schema(
+  {
+    productId: String,
+    yearlySalesTotal: Number,
+    yearlyTotalSoldUnits: Number,
+    year: Number,
+    monthlyData: [
+      {
+        month: String,
+        totalSales: Number,
+        totalUnits: Number,
+      },
+    ],
+    dailyData: [
+      {
+        date: String,
+        totalSales: Number,
+        totalUnits: Number,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const ProductStat = mongoose.model("ProductStat", ProductStatSchema);
+export default ProductStat;
